5c7c7de2c8ab1e6d8d3d1baa0f2d6d3dfbf4b5ce
Wolfgang Lutz <wlut@num42.de>
2026-02-09 17:04:32 +0100

    format

--- Sources/Internal/EnumCaseElementSyntax+Extensions.swift
+++ Sources/Internal/EnumCaseElementSyntax+Extensions.swift
@@ -1,15 +1,1 @@
-import SwiftSyntax
-
-extension EnumCaseElementSyntax {
-  var hasAssociatedValues: Bool {
-    !associatedValues.isEmpty
-  }
-
-  var associatedValues: [EnumCaseParameterSyntax] {
-    guard let parameterList = parameterClause?.parameters else {
-      return []
-    }
-
-    return parameterList.compactMap { $0 }
-  }
-}
+../../../../MacroAccessHelpers.swift
